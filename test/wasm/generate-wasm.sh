954ec8d48c4ea19aa2eba67c059bc9dc50517743
Masahiro Ide <masahiro.ide@linecorp.com>
2021-06-07 18:05:07 +0900

    Strip custom section from test code

--- test/wasm/generate-wasm.sh
+++ test/wasm/generate-wasm.sh
@@ -3,7 +3,9 @@
 CLANG=../llvm-project/build/bin/clang
 WASM_LD=../llvm-project/build/bin/wasm-ld
 OBJDUMP=../wabt/bin/wasm-objdump
+STRIP=../wabt/bin/wasm-strip
 
 $CLANG -Oz -target wasm32-wasm $1 -c -o $1.o
 $WASM_LD -o $1.wasm $1.o
 $OBJDUMP -d $1.wasm
+$STRIP $1.wasm
