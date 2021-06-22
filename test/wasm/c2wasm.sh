05aa1988ecd69976a3c681cb00df67db532fa2b6
Masahiro Ide <masahiro.ide@linecorp.com>
2021-06-22 18:33:23 +0900

    Add fibo test case

--- test/wasm/c2wasm.sh
+++ test/wasm/c2wasm.sh
@@ -4,8 +4,10 @@ CLANG=../llvm-project/build/bin/clang
 WASM_LD=../llvm-project/build/bin/wasm-ld
 OBJDUMP=../wabt/bin/wasm-objdump
 STRIP=../wabt/bin/wasm-strip
+#OPTS="-fno-optimize-sibling-calls "
+OPTS=""
 
-$CLANG -Oz -target wasm32-wasm $1 -c -o $1.o
+$CLANG -Oz $OPTS -target wasm32-wasm $1 -c -o $1.o
 $WASM_LD -o $1.wasm $1.o
 $STRIP $1.wasm
 $OBJDUMP -d $1.wasm
---
+++ test/wasm/fibo.c
@@ -0,0 +1,10 @@
+int fibo(int n) {
+  if(n < 3) {
+      return 1;
+  }
+  return fibo(n-1)+fibo(n-2);
+}
+
+int _start(int n) {
+    return fibo(n);
+}
